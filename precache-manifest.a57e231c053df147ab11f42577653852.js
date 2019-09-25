a8809ef07295820e23e93a697d7b1ff581b125eb
Sturza Mihai <sturzamihai23@gmail.com>
2019-09-25 11:07:29 +0300

    Updates

---
+++ precache-manifest.a57e231c053df147ab11f42577653852.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce887aef7b432ad5d028d0fe5b27f4a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52cf264e872b1cc7265f",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4f70cf622654b475f9c7",
+    "url": "/static/css/main.e53f7149.chunk.css"
+  },
+  {
+    "revision": "52cf264e872b1cc7265f",
+    "url": "/static/js/2.fa6a1981.chunk.js"
+  },
+  {
+    "revision": "4f70cf622654b475f9c7",
+    "url": "/static/js/main.5eeb6e4b.chunk.js"
+  },
+  {
+    "revision": "eb98a4f675d2e4824cff",
+    "url": "/static/js/runtime-main.a6aef668.js"
+  },
+  {
+    "revision": "3a36adf2077dfb20e918354b85fef009",
+    "url": "/static/media/bg.3a36adf2.png"
+  },
+  {
+    "revision": "a903e488396a738249cca00d1e225e71",
+    "url": "/static/media/logo.a903e488.png"
+  }
+]);
